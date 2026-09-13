e2646da82d173949c12452ea2e2ebec3552acbb6
Codephilia <riddhimashri150@gmail.com>
2026-09-13 23:35:46 +0530

    Add IssueNotFoundException Exception

---
+++ library/src/main/java/com/LibraryManagementSystem/library/exception/IssueNotFoundException.java
@@ -0,0 +1,7 @@
+package com.LibraryManagementSystem.library.exception;
+
+public class IssueNotFoundException extends RuntimeException {
+    public IssueNotFoundException(String message) {
+        super(message);
+    }
+}
